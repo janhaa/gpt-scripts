0009dbd1b6f5a8798964294dfae7c9768cc4815e
janhaa <jan.haeusler@rwth-aachen.de>
2023-04-22 01:42:42 +0200

    Update rotate-degrees.sh

--- ffmpeg/rotate-degrees.sh
+++ ffmpeg/rotate-degrees.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <input video> <degrees>"
+  exit 1
+fi
+
+input_video="$1"
+degrees="$2"
+output_file="${input_video%.*}-rotated.mp4"
+
+# Calculate the rotation direction based on the given degrees
+if [ "$degrees" -eq 90 ]; then
+  transpose="clock"
+elif [ "$degrees" -eq 270 ]; then
+  transpose="cclock"
+else
+  echo "Error: unsupported rotation degrees: $degrees"
+  exit 1
+fi
+
+# Rotate the input video by the given number of degrees
+ffmpeg -i "$input_video" -vf "transpose=$transpose" -metadata:s:v rotate="$degrees" -c:a copy "$output_file"
