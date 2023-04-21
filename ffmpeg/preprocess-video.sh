5811a0e59d1a04ce1cef4e5dbb43463e4abe64ad
janhaa <jan.haeusler@rwth-aachen.de>
2023-04-22 01:52:19 +0200

    Update preprocess-video.sh

--- ffmpeg/preprocess-video.sh
+++ ffmpeg/preprocess-video.sh
@@ -12,11 +12,13 @@ end_minute="$4"
 end_second="$5"
 degrees="$6"
 
-# Call the remove-rotation.sh script to remove rotation metadata
+# Remove any existing rotation metadata from the input video file
 ./remove-rotation.sh "$input_video"
 
-# Call the extract-from-clip.sh script to extract a clip from the video
-./extract-from-clip.sh "${input_video%-fixed.*}.mp4" "$start_minute" "$start_second" "$end_minute" "$end_second"
+# Extract the clip from the input video file
+clip_file="${input_video%.*}-clip.mp4"
+./extract-from-clip.sh "${input_video%-fixed.*}.mp4" "$start_minute" "$start_second" "$end_minute" "$end_second" "$clip_file"
 
-# Call the rotate-video.sh script to rotate the extracted clip
-./rotate-video.sh "${input_video%-fixed.*}-clip.mp4" "$degrees"
+# Rotate the extracted clip
+output_file="${clip_file%.*}-rotated.mp4"
+./rotate-video.sh "$clip_file" "$degrees" "$output_file"
